a0a826fc4b44f6f35ae0e0d87d97c0d297eebd12
sutarym <123245418+sutarym@users.noreply.github.com>
2023-08-22 11:39:38 +0530

    Update test_lambda.py

---
+++ test_lambda.py
@@ -0,0 +1,49 @@
+import requests
+import os
+import json
+import boto3
+
+def test_lambda():
+
+  request_body_ = {
+    'subnet_id': "",
+    'name': "Yasin",
+    'email': "sutaryasin243@gmail.com"
+  }
+
+  request_body = json.dumps(request_body_)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  request_headers = {
+        'X-Siemens-Auth': 'test'
+        
+    }
+
+  response = requests.post('https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data',
+                             headers=request_headers,
+                             data=json.dumps(request_body))
+ 
+  if response.status_code == 200:
+    return {
+      'statusCode': 200,
+      'body': json.dumps({'message': 'The request was successful.'})
+    }
+  else:
+    return {
+      'statusCode': response.status_code,
+      'body': json.dumps({'message': 'The request failed.'})
+    }
+
+

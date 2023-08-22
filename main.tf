34769461f9ce9e0623dc79dc46e61f9435fdf518
sutarym <123245418+sutarym@users.noreply.github.com>
2023-08-22 14:23:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,19 +72,21 @@ resource "aws_security_group" "SG" {
   ]
 
    ingress {
-     from_port = "0"
+#     from_port = "0"
  
-  to_port   = "0"
+#  to_port   = "0"
   protocol  = "-1"
   self      = true
  }
 
   egress {
-    from_port = "0"
-    to_port   = "0"
-    protocol  = "-1"
-    self      = "true"
+#     from_port        = 0
+#    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
   }
+}
 }
 
 

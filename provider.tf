21eb36d40b96f558f61d83caea512531f54853b4
sutarym <123245418+sutarym@users.noreply.github.com>
2023-08-22 13:27:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,6 @@
 
-///
-provider "aws" {
 
 
-region  = "ap-south-1"
-  assume_role {
-    role_arn     = data.aws_iam_role.lambda.arn
-    
-  }
-}
-///
 terraform {
   required_providers {
     aws = {

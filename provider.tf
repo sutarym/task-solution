af9bd479b055c417537c87225f7eaad72d370654
sutarym <123245418+sutarym@users.noreply.github.com>
2023-08-22 13:30:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
 }
-provider "aws" {
+provider "registry.terraform.io/hashicorp/aws" {
   region = "ap-south-1"
 assume_role {
     role_arn     = data.aws_iam_role.lambda.arn

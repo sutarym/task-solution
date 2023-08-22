3b192c10986b7eba0510e801e4394af688a2ec9c
sutarym <123245418+sutarym@users.noreply.github.com>
2023-08-22 12:41:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
-
-# Configure the AWS Provider
-# provider "aws" {
-#   region = "ap-south-1"
-# }
 
 
 

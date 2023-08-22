0ed8d0ade2a066b5ee9954be5af0c3075a160514
sutarym <123245418+sutarym@users.noreply.github.com>
2023-08-22 12:52:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,10 +6,10 @@ region  = "ap-south-1"
     
   }
 }
-terraform {
+
   backend "s3" {
     bucket = "3.devops.candidate.exam"
     key    = "Yasin.Sutar"
-    region = "ap-south-1"
+   # region = "ap-south-1"
   }
-}
+

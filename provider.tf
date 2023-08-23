e3f7273c1fce06805109ac7306ec96e25fa91d67
sutarym <123245418+sutarym@users.noreply.github.com>
2023-08-23 10:22:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 
 provider "aws" {   
   region = "ap-south-1"
+access_key = "AKIARN47KMZ7CBDA7FOU"
+  secret_key = "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"
 
     
   }
@@ -8,7 +10,7 @@ provider "aws" {
 
   terraform {
   backend "s3" {
-    bucket = "3.devops.candidate.exam"
+    bucket = "1.devops.candidate.exam"
     key    = "Yasin.Sutar"
     region = "ap-south-1"
   }

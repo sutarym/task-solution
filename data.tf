688f99a3bb5120accd920a758b2321140793ec28
sutarym <123245418+sutarym@users.noreply.github.com>
2023-08-23 10:17:21 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,11 +1,22 @@
+
 data "aws_nat_gateway" "nat" {
-  id = "nat-024f2855bf415a0cc"
+
+  id = "nat-0d688bbff8a47b274"
+
 }
+
+ 
 
 data "aws_vpc" "vpc" {
-  id = "vpc-04989d552e9a91336"
+
+  id = "vpc-00bf0d10a6a41600c"
+
 }
 
+ 
+
 data "aws_iam_role" "lambda" {
+
   name = "DevOps-Candidate-Lambda-Role"
+
 }
